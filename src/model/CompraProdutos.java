4abc9817f6412b0df93165e1b5236e8928516110
Daniel Affeldt <dnlaffeldt8@gmail.com>
2016-06-18 17:34:52 -0300

    atualização 14 /06

--- src/model/CompraProdutos.java
+++ src/model/CompraProdutos.java
@@ -15,6 +15,76 @@ public class CompraProdutos {
     private int codfornecedor;
     private int idDespesa;
     private float valor ;
+
+    /**
+     * @return the codigoproduto
+     */
+    public int getCodigoproduto() {
+        return codigoproduto;
+    }
+
+    /**
+     * @param codigoproduto the codigoproduto to set
+     */
+    public void setCodigoproduto(int codigoproduto) {
+        this.codigoproduto = codigoproduto;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the codfornecedor
+     */
+    public int getCodfornecedor() {
+        return codfornecedor;
+    }
+
+    /**
+     * @param codfornecedor the codfornecedor to set
+     */
+    public void setCodfornecedor(int codfornecedor) {
+        this.codfornecedor = codfornecedor;
+    }
+
+    /**
+     * @return the idDespesa
+     */
+    public int getIdDespesa() {
+        return idDespesa;
+    }
+
+    /**
+     * @param idDespesa the idDespesa to set
+     */
+    public void setIdDespesa(int idDespesa) {
+        this.idDespesa = idDespesa;
+    }
+
+    /**
+     * @return the valor
+     */
+    public float getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
     
     
 }
